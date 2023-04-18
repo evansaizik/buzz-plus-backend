0e74b547b67b8b3d44e79e7f7a8a989094a87e69
isaiah usiholo <evansaizik@gmail.com>
2023-04-18 17:38:17 +0100

    cors settings

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://buzz-plus-evansaizik.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
