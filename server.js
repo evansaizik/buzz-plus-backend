aef84b1d76e9bc9893c1dab455bab1e52d29e555
isaiah usiholo <evansaizik@gmail.com>
2023-04-18 17:29:51 +0100

    backend files

---
+++ server.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+const http = require('http');
+const server = http.createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('server is running'));
+const PORT = 8080;
+
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callended');
+  });
+
+  socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('calluser', { signal: signalData, from, name });
+  });
+
+  socket.on('answercall', (data) => {
+    io.to(data.to).emit('callaccepted', data.signal);
+  });
+});
+
+server.listen(PORT, () => console.log(`listening to request on port ${PORT}`));
